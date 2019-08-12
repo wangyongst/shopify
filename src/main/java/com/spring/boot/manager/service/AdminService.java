e3931208a667385099f5032f8dd141535cfff651
wangyongst <wangyongst@gmail.com>
2019-08-12 16:20:27 +0800

    create

---
+++ src/main/java/com/spring/boot/manager/service/AdminService.java
@@ -0,0 +1,4 @@
+package com.spring.boot.manager.service;
+
+public interface AdminService {
+}
---
+++ src/main/java/com/spring/boot/manager/service/impl/AdminServiceImpl.java
@@ -0,0 +1,17 @@
+package com.spring.boot.manager.service.impl;
+
+import com.spring.boot.manager.service.AdminService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@SuppressWarnings("All")
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = Exception.class, readOnly = false)
+public class AdminServiceImpl implements AdminService {
+
+    private static final Logger logger = LogManager.getLogger(AdminServiceImpl.class);
+}
