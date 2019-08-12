89647113555e0796184e4ebb65c778b1594a9736
wangyongst <wangyongst@gmail.com>
2019-08-12 16:41:35 +0800

    create

--- src/main/java/com/spring/boot/manager/admin/controller/AdminController.java
+++ src/main/java/com/spring/boot/manager/admin/controller/AdminController.java
@@ -2,7 +2,9 @@ package com.spring.boot.manager.admin.controller;
 
 
 import com.spring.boot.manager.service.AdminService;
+import com.spring.boot.manager.utils.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,10 @@ public class AdminController {
 
     @Autowired
     public AdminService adminService;
+
+    @GetMapping("/get/mailinfo")
+    public Result getMailinfo() {
+
+    }
+
 }
