abefd62a175f35fac147293fe21e39065cec5b25
wangyongst <wangyongst@gmail.com>
2019-08-12 16:27:59 +0800

    create

---
+++ src/main/java/com/spring/boot/manager/entity/Example.java
@@ -0,0 +1,33 @@
+package com.spring.boot.manager.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Example {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Basic
+    @Column(name = "url")
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/spring/boot/manager/entity/Size.java
@@ -0,0 +1,44 @@
+package com.spring.boot.manager.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Size {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Basic
+    @Column(name = "size")
+    private Double size;
+    @Basic
+    @Column(name = "symblesize")
+    private String symblesize;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public String getSymblesize() {
+        return symblesize;
+    }
+
+    public void setSymblesize(String symblesize) {
+        this.symblesize = symblesize;
+    }
+}
