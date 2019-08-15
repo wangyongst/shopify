0e0588383532df5df0c8a514de91aa8997317c4f
wangyongst <wangyongst@gmail.com>
2019-08-15 17:29:17 +0800

    create

--- src/main/java/com/spring/boot/manager/ManagerApplication.java
+++ src/main/java/com/spring/boot/manager/ManagerApplication.java
@@ -1,23 +1,10 @@
 package com.spring.boot.manager;
 
 
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.spring.boot.manager.repository"})
@@ -26,18 +13,6 @@ public class ManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-        return restTemplate;
     }
 }
 
--- src/main/java/com/spring/boot/manager/service/impl/AdminServiceImpl.java
+++ src/main/java/com/spring/boot/manager/service/impl/AdminServiceImpl.java
@@ -2,13 +2,22 @@ package com.spring.boot.manager.service.impl;
 
 import com.spring.boot.manager.repository.*;
 import com.spring.boot.manager.service.AdminService;
+import io.webfolder.cdp.Launcher;
+import io.webfolder.cdp.session.Session;
+import io.webfolder.cdp.session.SessionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Service
 @SuppressWarnings("All")
@@ -29,4 +38,28 @@ public class AdminServiceImpl implements AdminService {
     private SizeRepository sizeRepository;
 
 
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> command = new ArrayList<>();
+        //不显示google 浏览器
+        command.add("--headless");
+        Launcher launcher = new Launcher();
+        try (SessionFactory factory = launcher.launch(command);
+             Session session = factory.create()) {
+            try {
+                String url = "https://yeezysupply.com/products/carpenter-coat-oak?c=%2Fcollections%2Fnew-arrivals";
+                // 访问url
+                session.navigate(url);
+                // 等待页面加载完成，超时时间为30s
+                session.waitDocumentReady();
+                // 获取JS加载后的页面
+                String html = session.getContent();
+                session.selectInputText("M");
+                System.out.println(html);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
