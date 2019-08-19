7ba422a20e14c54cd1f9934ce37e62eaf9ba16bd
wangyongst <wangyongst@gmail.com>
2019-08-19 11:21:51 +0800

    create

--- src/main/java/com/spring/boot/manager/service/impl/AdminServiceImpl.java
+++ src/main/java/com/spring/boot/manager/service/impl/AdminServiceImpl.java
@@ -2,14 +2,29 @@ package com.spring.boot.manager.service.impl;
 
 import com.spring.boot.manager.repository.*;
 import com.spring.boot.manager.service.AdminService;
-import io.webfolder.cdp.Launcher;
-import io.webfolder.cdp.session.Session;
-import io.webfolder.cdp.session.SessionFactory;
+import org.apache.http.Consts;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -17,7 +32,8 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @SuppressWarnings("All")
@@ -38,27 +54,37 @@ public class AdminServiceImpl implements AdminService {
     private SizeRepository sizeRepository;
 
 
-    public static void main(String[] args) throws IOException {
-        ArrayList<String> command = new ArrayList<>();
-        //不显示google 浏览器
-        command.add("--headless");
-        Launcher launcher = new Launcher();
-        try (SessionFactory factory = launcher.launch(command);
-             Session session = factory.create()) {
+    public static void main(String[] args) throws Exception {
+        // 全局请求设置
+        RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
+        // 创建cookie store的本地实例
+        CookieStore cookieStore = new BasicCookieStore();
+        // 创建HttpClient上下文
+        HttpClientContext context = HttpClientContext.create();
+        context.setCookieStore(cookieStore);
+        // 创建一个HttpClient
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(globalConfig).setDefaultCookieStore(cookieStore).build();
+        CloseableHttpResponse res = null;
+        // 创建本地的HTTP内容
+        try {
             try {
-                String url = "https://yeezysupply.com/products/carpenter-coat-oak?c=%2Fcollections%2Fnew-arrivals";
-                // 访问url
-                session.navigate(url);
-                // 等待页面加载完成，超时时间为30s
-                session.waitDocumentReady();
-                // 获取JS加载后的页面
-                String html = session.getContent();
-                session.selectInputText("M");
-                System.out.println(html);
-            } catch (Exception e) {
-                e.printStackTrace();
+                // 构造post数据
+                List<NameValuePair> valuePairs = new LinkedList<NameValuePair>();
+                valuePairs.add(new BasicNameValuePair("checkout", "CHECK OUT"));
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(valuePairs, Consts.UTF_8);
+                entity.setContentType("application/x-www-form-urlencoded");
+                // 创建一个post请求
+                HttpPost post = new HttpPost("https://yeezysupply.com/cart");
+                // 注入post数据
+                post.setEntity(entity);
+                res = httpClient.execute(post, context);
+                for(Header header : res.getAllHeaders()){
+                        System.out.println(header.toString());
+                }
+            } finally {
+                httpClient.close();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
